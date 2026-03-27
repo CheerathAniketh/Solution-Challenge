88ae1d9acfd73f1be48e753b77b69468d4756aa3
CheerathAniketh <cheerathaniketh@gmail.com>
2026-03-27 08:31:19 +0530

    Using RF instead of LogisticRegressor

--- backend/trainer.py
+++ backend/trainer.py
@@ -1,7 +1,8 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+
 
 def prepare_features(df, target_col, sensitive_col):
     df_encoded = df.copy()
@@ -13,22 +14,18 @@ def prepare_features(df, target_col, sensitive_col):
     y = df_encoded[target_col]
     return X, y
 
+
 def train_and_evaluate(df, target_col, sensitive_col):
     X, y = prepare_features(df, target_col, sensitive_col)
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
-    
-    # scale features — fixes convergence on large datasets
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    
-    # convert back to DataFrame to keep column names for SHAP
-    import numpy as np
-    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns)
-    X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test.columns)
-    
-    model = LogisticRegression(max_iter=3000, random_state=42)
-    model.fit(X_train_scaled, y_train)
-    
-    return model, X_train_scaled, X_test_scaled, y_test
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=8,
+        random_state=42,
+        n_jobs=-1  # use all CPU cores
+    )
+    model.fit(X_train, y_train)
+
+    return model, X_train, X_test, y_test
